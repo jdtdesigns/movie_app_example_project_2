18db9bcb9af793c0f1acd30b72b46efa597cef22
JD Tadlock <jt7903@gmail.com>
2024-08-01 04:07:18 -0400

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,14 @@
 const { Sequelize } = require('sequelize');
+const is_prod = process.env.NODE_ENV === 'production';
 
-const client = new Sequelize(
+const client = is_prod ? new Sequelize(process.env.DB_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+}) : new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD, {
